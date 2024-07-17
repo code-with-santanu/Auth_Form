a69e9dcc7dd81fcef3b417506fb6a0913ce22313
code-with-santanu <santanupal003@gmail.com>
2024-07-18 01:44:01 +0530

    Toaster and auth context fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <Toaster position="top-center" />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
