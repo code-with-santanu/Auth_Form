7fa008e40320741120d1b373850ce40d5ff08381
code-with-santanu <santanupal003@gmail.com>
2024-07-18 00:26:17 +0530

    navbar added

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -7,37 +7,45 @@ import Login from "../components/login_component/login";
 import Register from "../components/signup_component/signup";
 import loginImg from "../assests/login.png";
 import signupImg from "../assests/signup.png";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   // To show of hide the login and reg form
   const [show, setShow] = useState(true);
 
+  const changeState = (show) => {
+    setShow(show);
+  };
+
   return (
-    <div className="w-full h-full md:h-screen outer-div flex justify-between	  	">
-      {show && (
-        <>
-          <Image
-            src={loginImg}
-            alt="logo"
-            height="500"
-            width="auto"
-            className=" mr-36 "
-          />
-          <Login onClick={(show) => setShow(show)} />
-        </>
-      )}
-      {!show && (
-        <>
-          <Register onClick={(show) => setShow(show)} />
-          <Image
-            src={signupImg}
-            alt="logo"
-            height="380"
-            width="auto"
-            className=" ml-40 "
-          />
-        </>
-      )}
-    </div>
+    <>
+      <Navbar onClick={changeState} />
+      <div className="w-full h-full md:h-screen outer-div flex justify-between">
+        {show && (
+          <>
+            <Image
+              src={loginImg}
+              alt="logo"
+              height="500"
+              width="auto"
+              className=" mr-36 "
+            />
+            <Login onClick={changeState} />
+          </>
+        )}
+        {!show && (
+          <>
+            <Register onClick={changeState} />
+            <Image
+              src={signupImg}
+              alt="logo"
+              height="380"
+              width="auto"
+              className=" ml-40 "
+            />
+          </>
+        )}
+      </div>
+    </>
   );
 }
