8d345f33c40353c04ff2ef2d356f0adab9a5a1f4
code-with-santanu <santanupal003@gmail.com>
2024-07-20 23:18:43 +0530

    updated

--- src/app/components/DialogBox.jsx
+++ src/app/components/DialogBox.jsx
@@ -5,7 +5,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Transition } from "../requirement";
+import { Transition } from "../helper/requirement";
 export default function AlertDialogSlide({
   stateOpen,
   onClose,
