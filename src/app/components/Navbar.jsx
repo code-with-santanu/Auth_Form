ff6458c43524f25fc0b492b3bcaca87b52895f45
code-with-santanu <santanupal003@gmail.com>
2024-07-20 23:20:15 +0530

    heading text added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     <div className="w-full h-16 mr-10 bg-yellow-300 flex justify-end items-center ">
       <div className="nav-btn flex justify-between">
         <div
-          className="btn w-20 h-9 pt-1.5 mx-4 bg-white rounded-lg border-solid border-black text-center font-semibold"
+          className="btn w-20 h-9 pt-1.5 mx-4 bg-white rounded-lg border-solid border-black text-center font-semibold cursor-pointer"
           onClick={(e) => {
             e.preventDefault();
             props.onClick(false);
@@ -14,7 +14,7 @@ export default function Navbar(props) {
           SIGNUP
         </div>
         <div
-          className="btn w-20 h-9 pt-1.5 mx-4 bg-white rounded-lg border-solid border-black text-center font-semibold"
+          className="btn w-20 h-9 pt-1.5 mx-4 bg-white rounded-lg border-solid border-black text-center font-semibold cursor-pointer"
           onClick={(e) => {
             e.preventDefault();
             props.onClick(true);
