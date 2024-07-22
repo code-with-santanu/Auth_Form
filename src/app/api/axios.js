b35748286485df948a54c45675439a87ec8ee1db
code-with-santanu <santanupal003@gmail.com>
2024-07-22 21:09:49 +0530

    server url changed

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://authenticationsystem-ttz8.onrender.com",
+  baseURL: "https://authenticationsystem.up.railway.app",
   mode: "cors",
 });
